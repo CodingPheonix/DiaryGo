88b1a2466d8a9e2922cceaa3eb74e130a5fa542d
Sayan Biswas <168727255+CodingPheonix@users.noreply.github.com>
2025-08-31 17:41:27 +0530

    connected environment variables

--- DiaryGo - app/app/Components/APIs.ts
+++ DiaryGo - app/app/Components/APIs.ts
@@ -1,68 +1,9 @@
-
-// // export const create_task_list = async (body) => {
-// //     const response = await fetch(`/api/create_taskList_agent`, {
-// //         method: 'POST',
-// //         headers: {
-// //             'Content-Type': 'application/json'
-// //         },
-// //         body: JSON.stringify(body)
-// //     });
-// //     const result = await response.json();
-// //     return { response, result };
-// // };
-
-
-// // lib/api.ts
-// export async function sendMessage(message: string) {
-//     const res = await fetch("/api/agent", {
-//         method: "POST",
-//         headers: { "Content-Type": "application/json" },
-//         body: JSON.stringify({ message }),
-//     });
-
-//     if (!res.ok) {
-//         throw new Error("Failed to connect to API");
-//     }
-
-//     // Because server returns a ReadableStream (SSE-style),
-//     // we need to read line-by-line.
-//     const reader = res.body?.getReader();
-//     const decoder = new TextDecoder();
-//     let buffer = "";
-
-//     if (!reader) return [];
-
-//     const messages = [];
-
-//     while (true) {
-//         const { done, value } = await reader.read();
-//         if (done) break;
-
-//         buffer += decoder.decode(value, { stream: true });
-
-//         // Split by newline since your server sends JSON per line
-//         const parts = buffer.split("\n");
-//         buffer = parts.pop() || "";
-
-//         for (const part of parts) {
-//             if (!part.trim()) continue;
-//             try {
-//                 const parsed = JSON.parse(part);
-//                 messages.push(parsed);
-//             } catch {
-//                 console.warn("Failed to parse chunk:", part);
-//             }
-//         }
-//     }
-
-//     return messages;
-// }
-
-const backendUrl = process.env.BACKEND_URL;
+const agentURL = process.env.NEXT_PUBLIC_AGENT_URL;
+console.log(agentURL)
 
 export async function sendMessage(message: string) {
     try {
-        const response = await fetch(`http://127.0.0.1:8000/graph`, {
+        const response = await fetch(`${agentURL}/graph`, {
             method: "POST",
             headers: { "Content-Type": "application/json" },
             body: JSON.stringify({ user_input: message }),
